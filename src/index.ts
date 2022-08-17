094c38712d17c7feee86ed0e34eb56a1e93fa5a6
gabrielJGS <gabrielftjose@gmail.com>
2022-08-17 14:50:03 -0300

    feat: added category route to index file

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import errorHandler from "./middlewares/error-handler.middleware";
 import authorizationRoute from "./routes/authorization.route";
 import statusRoute from "./routes/status.route";
 import usersRoute from "./routes/users.route";
+import categoriesRoute from "./routes/categories.route";
 
 const app = express();
 const PORT = 3000;
@@ -14,6 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(statusRoute);
 app.use(authorizationRoute);
 app.use(usersRoute);
+app.use(categoriesRoute);
+
 // app.use(jwtAuthenticationMiddleware, usersRoute);
 app.use(errorHandler);
 
