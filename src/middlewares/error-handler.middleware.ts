f7f9dc37a423152840e4a84d1105c5ad9acbed23
gabrielJGS <gabrielftjose@gmail.com>
2022-08-17 14:46:37 -0300

    feat: added unathorized model to error handler middleware

--- src/middlewares/error-handler.middleware.ts
+++ src/middlewares/error-handler.middleware.ts
@@ -2,16 +2,18 @@ import { NextFunction, Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import DatabaseError from "../models/errors/database.error.model";
 import ForbiddenError from "../models/errors/forbidden.error.model";
+import UnauthorizedError from "../models/errors/unathorized.error.model";
 
 function errorHandler(error: any, req: Request, res: Response, next: NextFunction) {
   if (error instanceof DatabaseError) {
-    res.send(error).status(StatusCodes.BAD_REQUEST);
+    res.status(StatusCodes.BAD_REQUEST).send(error);
   } else if (error instanceof ForbiddenError) {
-    res.send(error).status(StatusCodes.FORBIDDEN);
+    res.status(StatusCodes.FORBIDDEN).json(error);
+  } else if (error instanceof UnauthorizedError) {
+    res.status(StatusCodes.UNAUTHORIZED).json(error);
   } else {
-    res.send(error).status(StatusCodes.INTERNAL_SERVER_ERROR);
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send(error);
   }
-
 }
 
 export default errorHandler;
