004d75a8e91591d557d176ae3b82faad3ce72223
gabrielJGS <gabrielftjose@gmail.com>
2022-08-18 13:50:48 -0300

    fix: added more options to db connection

--- src/db/datasource.ts
+++ src/db/datasource.ts
@@ -1,14 +1,15 @@
 import "reflect-metadata";
 require("dotenv/config");
-import { DataSource } from "typeorm";
+import { DatabaseType, DataSourceOptions, DataSource } from "typeorm";
 import { User } from "../entities/User";
 import { Category } from "../entities/Category";
 import { Product } from "../entities/Product";
 
-const { DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_DB } = process.env;
+const { DB_TYPE, DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_DB } = process.env;
 
-const AppDataSource = new DataSource({
-  type: "postgres",
+const type = DB_TYPE ? (DB_TYPE as DatabaseType) : "postgres";
+const databaseOptions = {
+  type: type,
   host: DB_HOST,
   port: Number(DB_PORT),
   username: DB_USER,
@@ -17,12 +18,13 @@ const AppDataSource = new DataSource({
   entities: [User, Category, Product],
   synchronize: true,
   logging: false,
-});
+} as DataSourceOptions;
+const AppDataSource = new DataSource(databaseOptions);
+
+AppDataSource.initialize().catch((error) => console.log(error));
 
 // to initialize initial connection with the database, register all entities
 // and "synchronize" database schema, call "initialize()" method of a newly created database
 // once in your application bootstrap
-
-AppDataSource.initialize().catch((error) => console.log(error));
 
 export { AppDataSource };
